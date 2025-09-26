0512b7cdf405c5e1d63004415e264a86cef46939
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-09-26 16:19:26 +0200

    add second part of day3

--- day3/src/Main.java
+++ day3/src/Main.java
@@ -1,8 +1,13 @@
 static final String FILE_PATH = "src/input.txt";
+static enum Occurence {
+    MOST,
+    LEAST
+}
 
 void main() {
     try {
         System.out.println(firstPart());
+        System.out.println(secondPart());
     } catch (IOException e) {
         e.printStackTrace();
     }
@@ -54,5 +59,78 @@ private static int firstPart() throws IOException {
         int epsilonRate = Integer.parseInt(epsilonRateString, 2);
 
         return gammaRate * epsilonRate;
+    }
+}
+
+private static int secondPart() throws IOException {
+    List<String> binaryCodes = Files.lines(Paths.get(FILE_PATH))
+            .map(String::trim)
+            .filter(line -> !line.isBlank())
+            .toList();
+
+    List<String> binaryCodesCopy = new ArrayList<>(binaryCodes);
+
+    while (binaryCodesCopy.size() > 1) {
+
+        for (int i = 0; i < binaryCodes.getFirst().length(); i++) {
+            final int position = i;
+            char character = findOccurence(position, '1', Occurence.MOST, binaryCodesCopy);
+            binaryCodesCopy = binaryCodesCopy.stream().filter(code -> code.charAt(position) == character).toList();
+
+            if (binaryCodesCopy.size() == 1) {
+                break;
+            }
+        }
+    }
+
+    final int oxygenGeneratorRating = Integer.parseInt(binaryCodesCopy.getFirst(), 2);
+
+    while (binaryCodes.size() > 1) {
+
+        for (int i = 0; i < binaryCodes.getFirst().length(); i++) {
+            final int position = i;
+            char character = findOccurence(position, '0', Occurence.LEAST, binaryCodes);
+            binaryCodes = binaryCodes.stream().filter(code -> code.charAt(position) == character).toList();
+
+            if (binaryCodes.size() == 1) {
+                break;
+            }
+        }
+    }
+
+    final int co2ScrubberRating = Integer.parseInt(binaryCodes.getFirst(), 2);
+
+    return oxygenGeneratorRating * co2ScrubberRating;
+}
+
+private static char findOccurence(int bitIndex, char fallbackBitIfEquals, Occurence occurence, List<String> codes) {
+    int count = 0;
+
+    for (int i = 0; i < codes.size(); i++) {
+        String code = codes.get(i);
+
+        if (code.charAt(bitIndex) == '1') {
+            count++;
+        }
+    }
+
+    switch (occurence) {
+        case MOST:
+            if (count > (codes.size() - count)) {
+                return '1';
+            }
+            break;
+
+        case LEAST:
+            if (count < (codes.size() - count)) {
+                return '1';
+            }
+            break;
+    }
+
+    if (count == (codes.size() - count)) {
+        return fallbackBitIfEquals;
+    } else {
+        return '0';
     }
 }
