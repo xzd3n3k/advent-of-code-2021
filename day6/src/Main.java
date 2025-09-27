6d4dc4ec0162fbfa4393471e9a643da35085250c
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-09-27 16:56:39 +0200

    add part two of day6, completely remake solution to be able to work wit billion numbers

--- day6/src/Main.java
+++ day6/src/Main.java
@@ -4,6 +4,35 @@ void main() {
     try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
 
         String line = br.readLine();
+        long[] fishCounts = new long[9];
+
+        Arrays.stream(line.split(","))
+                .mapToInt(Integer::parseInt)
+                .forEach(timer -> fishCounts[timer]++);
+
+        for (int day = 0; day < 256; day++) {
+            long newFish = fishCounts[0];
+
+            for (int i = 0; i < 8; i++) {
+                fishCounts[i] = fishCounts[i + 1];
+            }
+
+            fishCounts[6] += newFish;
+            fishCounts[8] = newFish;
+        }
+
+        long total = Arrays.stream(fishCounts).sum();
+        System.out.println(total);
+
+    } catch (IOException e) {
+        System.out.println("Error reading file.");
+    }
+}
+
+private static void slowSolution() {
+    try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+
+        String line = br.readLine();
         List<Integer> fishLives = Arrays.stream(line.split(","))
                 .map(Integer::parseInt)
                 .toList();
