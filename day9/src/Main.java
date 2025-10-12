4da7edc7294e5f0ac541e9cf788a624db6509cd8
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-10-12 12:51:41 +0200

    add part two of day9

--- day9/src/Main.java
+++ day9/src/Main.java
@@ -5,6 +5,7 @@ void main() {
 
         String line;
         List<List<Integer>> fieldMap = new ArrayList<List<Integer>>();
+        List<Integer> basins = new ArrayList<Integer>();
 
         while ((line = br.readLine()) != null) {
             fieldMap.add(Arrays.stream(line.split("")).map(Integer::parseInt).toList());
@@ -17,11 +18,25 @@ void main() {
                 int currentNumber = fieldMap.get(i).get(j);
 
                 if (isLowerThanNeighbors(currentNumber, i, j, fieldMap)) {
+                    Set<String> visited = new HashSet<>();
+                    visited.add(i + "," + j);
+                    basins.add(findOneHigher(currentNumber, i, j, fieldMap, visited)+1);
                     riskLevelsSum += currentNumber+1;
                 }
             }
         }
 
+        basins.sort(Comparator.reverseOrder());
+
+        System.out.println(basins);
+
+        int basinsTogether = 1;
+
+        for (int i = 0; i < 3; i++) {
+            basinsTogether *= basins.get(i);
+        }
+
+        System.out.println(basinsTogether);
         System.out.println(riskLevelsSum);
 
     } catch (IOException e) {
@@ -55,4 +70,40 @@ private static boolean isLowerThanNeighbors(int number, int row, int col, List<L
 
     // If no neighbor is less or equal, then this is a low point
     return true;
+}
+
+private static int findOneHigher(int number, int row, int col,
+                                 List<List<Integer>> fieldMap, Set<String> visited) {
+    int rows = fieldMap.size();
+    int cols = fieldMap.get(row).size();
+    int count = 0;
+
+    int[][] directions = {
+            {-1, 0}, // up
+            {1, 0},  // down
+            {0, -1}, // left
+            {0, 1}   // right
+    };
+
+    for (int[] dir : directions) {
+        int newRow = row + dir[0];
+        int newCol = col + dir[1];
+
+        if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols)
+            continue;
+
+        int neighborValue = fieldMap.get(newRow).get(newCol);
+        String coordKey = newRow + "," + newCol;
+
+        if (!visited.contains(coordKey)
+                && neighborValue != 9
+                && neighborValue > number) {
+
+            visited.add(coordKey);
+            count++;
+            count += findOneHigher(neighborValue, newRow, newCol, fieldMap, visited);
+        }
+    }
+
+    return count;
 }
