fa40e0b4b3ad7a605078c103022252ed6d652d85
Zdeněk Němec <xzdeneknemec@outlook.cz>
2026-02-15 19:29:34 +0100

    add part two of day10

--- day10/src/Main.java
+++ day10/src/Main.java
@@ -1,55 +1,87 @@
 static final String FILE_PATH = "src/input.txt";
 static final String OPENING_OPERATORS = "({[<";
 static final String CLOSING_OPERATORS = ")}]>";
-static final Map<Character, Integer> CHAR_VALUE_MAP = Map.of(
+static final Map<Character, Integer> CHAR_VALUE_MAP_PART1 = Map.of(
     ')', 3,
     ']', 57,
     '}', 1197,
     '>', 25137
 );
+static final Map<Character, Integer> CHAR_VALUE_MAP_PART2 = Map.of(
+    ')', 1,
+    ']', 2,
+    '}', 3,
+    '>', 4
+);
+
+public record ValidationResult(Character incorrectChar, double value) {};
 
 void main() {
   try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
 
     int value = 0;
+    List<Double> values = new ArrayList<Double>();
     String line;
 
     while ((line = br.readLine()) != null) {
-      Character incorrectChar = findFirstIncorrectClosingCharacter(line.toCharArray());
+      ValidationResult validationResult = validateLine(line.toCharArray());
+      Character incorrectChar = validationResult.incorrectChar;
 
       if (incorrectChar != null) {
-        value += CHAR_VALUE_MAP.get(incorrectChar);
+        value += CHAR_VALUE_MAP_PART1.get(incorrectChar);
+      } else {
+        values.add(validationResult.value);
       }
     }
 
-    IO.println(value);
+    Collections.sort(values);
 
+    System.out.printf("Part1 value: %d%n", value);
+    System.out.printf("Part 2 value: %f%n", values.get(values.size() / 2));
   } catch (IOException e) {
     IO.println("Error reading file.");
   }
 }
 
-private static Character findFirstIncorrectClosingCharacter(char[] characters) {
+private static ValidationResult validateLine(char[] characters) {
   var leftOperatorStack = new ArrayList<Character>();
-  var rightOperatorStack = new ArrayList<Character>();
   Character invalidOperator = null;
+  double valuePart2 = 0;
 
-  for (int x = 0; x < characters.length; x++) {
-    if (OPENING_OPERATORS.contains("" + characters[x])) {
-      leftOperatorStack.add(characters[x]);
-    } else if (CLOSING_OPERATORS.contains("" + characters[x])) {
-      if (leftOperatorStack.getLast().equals(getOppositeOperator(characters[x]))) {
+  // finds first incorrect closing character if exists
+  for (char value : characters) {
+    // if character is opening operator, it puts him into stack
+    if (OPENING_OPERATORS.contains("" + value)) {
+      leftOperatorStack.add(value);
+      // if character is closing operator, looks at top of the stack and if its opposing operator, it pops
+    } else if (CLOSING_OPERATORS.contains("" + value)) {
+      if (leftOperatorStack.getLast().equals(getOppositeOperator(value))) {
         leftOperatorStack.removeLast();
+        // else its the incorrect closing operator so it just stores it and break the cycle
       } else {
-        rightOperatorStack.add(characters[x]);
-        System.out.printf("Expected %c, but found %c instead.%n", getOppositeOperator(leftOperatorStack.getLast()), characters[x]);
-        invalidOperator = characters[x];
+        System.out.printf("Expected %c, but found %c instead.%n", getOppositeOperator(leftOperatorStack.getLast()), value);
+        invalidOperator = value;
         break;
       }
     }
   }
 
-  return invalidOperator;
+  // if no invalid operator was found, then there are some missing operators
+  if (invalidOperator == null) {
+    // replace operators with opposite ones
+    leftOperatorStack = leftOperatorStack
+        .reversed()
+        .stream()
+        .map(c -> getOppositeOperator(c))
+        .collect(Collectors.toCollection(ArrayList::new));
+
+    for (Character character : leftOperatorStack) {
+      valuePart2 *= 5;
+      valuePart2 += CHAR_VALUE_MAP_PART2.get(character);
+    }
+  }
+
+  return new ValidationResult(invalidOperator, valuePart2);
 }
 
 private static Character getOppositeOperator(char character) {
